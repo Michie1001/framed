831fe6fde46bf0ea353ba95462a7e000106185ab
Michie1001 <michieugwu@gmail.com>
2020-09-24 23:58:01 +0100

    rearrange

--- dashboard/resources/views/main.blade.php
+++ dashboard/resources/views/main.blade.php
@@ -1,1 +1,13 @@
 {{-- main dashboard --}}
+.............
+<!-- This would be the dashboard app
+Sections include:
+-A to-do 
+-Weather forecast
+-Day counter for my Year in Colors
+-Calender date
+-Music player
+-A random muse photo from pinterest
+-Clock
+-News 
+-Fun fact like Curiosity Stream -->
