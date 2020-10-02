523a3188dc6d00561a51a862090537c40f911cb4
Michie1001 <michieugwu@gmail.com>
2020-10-02 23:27:49 +0100

    locate css please?

--- dashboard/resources/views/main.blade.php
+++ dashboard/resources/views/main.blade.php
@@ -15,7 +15,14 @@ Sections include:
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<title>Framed</title>
+	
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<link href="/css/main.css" rel="stylesheet">
+
 </head>
 <body>
 	<div class="background">
